b5e9cd073342692840df233fb88a15b246f3e416
arbin-meier <122821852+arbin-meier@users.noreply.github.com>
2023-02-16 15:03:53 -0600

    AR-22 Meier dev branch (#1)
    * Create Hello_World.cs
    
    nothing else

---
+++ Hello_World.cs
@@ -0,0 +1,9 @@
+//Please pretend this is a well coded file, and you are going to modify it
+public class Myclass
+{
+    //my fields
+    public int Id { get; set; }
+    public string Name { get; set; }
+
+    public static void myMethod();
+}

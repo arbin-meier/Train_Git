3a1c445ebb9a315b2a6d12c2027e8cbd3e3794c1
Rongyu Cui <rongyu.c@arbin.com>
2023-02-16 15:14:10 -0600

    Update Hello_World.cs
    Rongyu
    Age

---
+++ Hello_World.cs
@@ -0,0 +1,11 @@
+//Please pretend this is a well coded file, and you are going to modify it
+public class Myclass
+{
+    //my fields
+    public int Age { get; set; }
+    public string Name { get; set; }
+
+    public static void myMethod();
+
+    public static void Rongyu();
+}
